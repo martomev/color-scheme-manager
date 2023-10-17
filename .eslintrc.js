1f52382559258613765dfd2793685059dd40fe16
Martin Metodiev <martin@metodiev.dev>
2023-10-17 09:12:22 +0300

    Add no-console & no-debugger ESLint rules based on BUILD_MODE

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const DEV_MODE = process.env.BUILD_MODE === 'development';
+
 module.exports = {
   env: {
     browser: true
@@ -5,11 +7,12 @@ module.exports = {
   plugins: ['prettier'],
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
   rules: {
-    // Add any project-specific ESLint rules here
     'comma-dangle': ['error', 'never'],
     'no-underscore-dangle': 'off',
     'no-constructor-return': 'off',
-    'class-methods-use-this': 'off'
+    'class-methods-use-this': 'off',
+    'no-console': DEV_MODE ? 'off' : ['warn'],
+    'no-debugger': DEV_MODE ? 'off' : ['warn']
   },
   parserOptions: {
     ecmaVersion: 13
