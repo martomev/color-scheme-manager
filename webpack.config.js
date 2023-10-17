36bff72be9038faf1c8d0d007e4c1e856d20bfeb
Martin Metodiev <martin@metodiev.dev>
2023-10-17 09:13:08 +0300

    Refine Webpack config with TerserPlugin and BUILD_MODE

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,51 @@
 const path = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = (env, options) => {
-  return {
-    entry: './index.js',
-    output: {
-      filename: 'color-scheme-manager.js',
-      path: path.resolve(__dirname, 'dist'),
-      libraryTarget: 'commonjs2'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader'
-          }
+const DEV_MODE = process.env.BUILD_MODE === 'development';
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'color-scheme-manager.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
         }
-      ]
-    },
-    resolve: {
-      extensions: ['.js']
-    },
-    plugins: [new ESLintPlugin({})],
-    devtool: options.mode === 'development' ? 'inline-source-map' : false,
-    devServer: {
-      port: 8000 // Set the desired port number here
-    }
-  };
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  plugins: [new ESLintPlugin({})],
+  devtool: DEV_MODE ? 'inline-source-map' : false,
+  devServer: {
+    port: 8000 // Set the desired port number here
+  },
+  optimization: {
+    minimize: !DEV_MODE,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          mangle: false,
+          compress: {
+            drop_debugger: !DEV_MODE,
+            drop_console: !DEV_MODE
+          },
+          output: {
+            comments: false
+          }
+        },
+        extractComments: false
+      })
+    ]
+  }
 };
